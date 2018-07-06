529feeaf34f4c887e04b973d4228ef7393343cb2
rise-mike <mike.morganti@risepeople.com>
2018-07-06 10:19:56 -0700

    Working list of projects - styling needed

---
+++ app.js
@@ -0,0 +1,48 @@
+var chartApp = angular.module('chartApp', ['ngRoute']);
+
+chartApp.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/chart.html',
+    controller: 'chartController'
+  })
+  .when('/projects', {
+    templateUrl: 'views/projects.html',
+    controller: "projectController"
+  })
+  .when('/specialists', {
+    templateUrl: 'views/specialists.html',
+    controller: 'specialistController'
+  });
+});
+
+chartApp.controller('chartController', ['$scope', '$http', function($scope, $http) {
+
+//   $scope.chartData = {}
+
+//   $http.get('chart/list.json').success(function(response) {
+//     console.log("RESPONSE: ", response)
+//     $scope.chartData = response
+//   })
+// console.log("On the scope ", $scope.chartData)
+
+}])
+
+chartApp.controller('projectController', ['$scope', '$http', function($scope, $http) {
+
+  $scope.projects = []
+  
+  $http.get('chart/output.json').success(function(response) {
+    $scope.projects = response
+  })
+
+  $scope.getWidth = function(project){
+    elementVal = String(project.progress) + "%"
+    return elementVal
+  }
+
+}])
+
+chartApp.controller('specialistController', ['$scope', '$http', function($scope, $http) {
+
+}])
--- scripts/batch_fetch.py
+++ scripts/batch_fetch.py
@@ -1,62 +1,98 @@
 import requests
 import json
-
-config = {
-    "domain": "https://projectsapi.zoho.com/restapi/portal/risepeople/projects/",
-    "auth": "?authtoken=a62f7e0580c4233f34740979bd234442",
-    "params": "test"
-}
+from config import projectsConfig
+import datetime
 
 def fetchAllProjects(config):
     # Fetches from the supplied url using a specified token.
     url = config['domain'] + config['auth']
-    return requests.get(url).json()
-    # writeJson(requests.get(url).json(), "test_dump.json")
-
-def writeProjectToJson(jsonObj, dest):
-    # Accepts a JSON object, and dumps it into a specified destination file.
-    with open(dest, "w") as outfile:
-        json.dump(jsonObj, outfile)
+    zohoResponse = requests.get(url, params=config['params']).json()
+    return zohoResponse
 
 def parseAllProjects(rawProjects):
-    # Takes in a dict of "rawProjects" from Zoho Projects, returns a dict of parsed projects 
-    allParsedProjects = {}
+    # Takes in a dict of "rawProjects" from Zoho Projects, returns a list of parsed projects 
+    allParsedProjects = []
     for aProject in rawProjects:
-        currentProjectName = (aProject["name"])
-        allParsedProjects[currentProjectName] : singleProjectBuilder(aProject)
-        print("IN THE FOR LOOP:", singleProjectBuilder(aProject))
-        print("Parsed, in loop ", allParsedProjects)
-
-    print("All Parsed Projies: ", allParsedProjects)
+        currentProject = (singleProjectBuilder(aProject))
+        allParsedProjects.append(currentProject)
+    return allParsedProjects
     
-
 def singleProjectBuilder(rawProject):
     # Takes a single "rawProject" Zoho Project JSON object, returns a parsed object, "parsedProject"
     parsedProject = {
-        rawProject["name"] : {
+            "name": rawProject["name"],  
             "projectId": rawProject["id"],
             "owner": rawProject["owner_name"],
             "startDate": rawProject["start_date"],
-            # "goLive": rawProject["custom_fields"][5]["Expected go-live date"],
+            "custom": customIsolateSingle(rawProject["custom_fields"]),
             "tasks_open": rawProject["task_count"]["open"],
             "tasks_closed": rawProject["task_count"]["closed"],
             "status": rawProject["status"],
-        }
+            "progress" : str(progresCalc(rawProject["task_count"]["open"], rawProject["task_count"]["closed"])),
+            "progress_val" : progresCalc(rawProject["task_count"]["open"], rawProject["task_count"]["closed"])
     }
+    # customFieldParser(parsedProject["custom"])
+    # return arrayProjectBuilder(parsedProject)
+
     return parsedProject
 
+def arrayProjectBuilder(projy):
+    arraydProj = [None, projy["name"], projy["owner"], projy["startDate"], projy["custom"]["Expected go-live date"], None, projy["progress"], None]
+    return arraydProj
 
 
+# def customFieldParser(projectsDict):
+#     customFieldDict = {}
+#     for projdict in projectsDict:
+#         customFieldDict[projdict["name"]] = customFieldIsolate(projdict["custom"])
+    
+#     writeProjectToJson(customFieldDict, "custom_export.json")
+    
+# def customFieldIsolate(custom):
+#     retproj = {}
+#     for thing in custom:
+#         for key, value in thing.items():
+#             dummy = {key: value}
+#             retproj[key] = value
 
-allProjects = fetchAllProjects(config)
-isolatedProjects = allProjects["projects"]
+#     return retproj
 
-print(parseAllProjects(isolatedProjects))
+def customIsolateSingle(customObj):
+    returnCustom = {}
+    for dicty in customObj:
+        for key, value in dicty.items():
+            returnCustom[key] = value
 
-# print("RETURNED BOYE: ", singleProjectBuilder(allProjects["projects"][1]))
+    return returnCustom
 
-# oneProj = retObj["projects"][0]
-# print("FROM API: ", oneProj)
-# projBuild(oneProj)
+def progresCalc(open, closed):
+    if closed == 0:
+        return 0
+    else:
+        total = open + closed
+        return round(closed/total*100)
 
+# def durationCalc(start, goLiveDate):
+    
+def writeProjectToJson(jsonObj, dest):
+    # Accepts a JSON object, and dumps it into a specified destination file.
+    print("Writing to ", dest)
+    with open(dest, "w") as outfile:
+        json.dump(jsonObj, outfile)
 
+zohoDump = fetchAllProjects(projectsConfig)
+isolatedProjects = zohoDump["projects"]
+print("Parsing:", len(isolatedProjects), "projects")
+parsedProjects = parseAllProjects(isolatedProjects)
+
+def projList(projects):
+    returnList= []
+    for projj in parsedProjects:
+        returnList.append([None, projj["name"], projj["owner"], projj["startDate"], None, projj["progress"], None])
+    return returnList
+
+# writeProjectToJson(projList(parsedProjects), "../chart/list.json")
+
+writeProjectToJson(parsedProjects, "../chart/output.json")
+
+# customFieldParser(parsedProjects)
